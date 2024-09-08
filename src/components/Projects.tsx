40f64c3c75cc0049ea1c6e8a41bfcd587a89e10e
Frank <wanyoike39@gmail.com>
2024-09-08 23:08:22 +0300

    add the rest of the projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,11 @@
 import '../css/projects.css';
 import chess from '../assets/ranci-chess.png';
 import todo from '../assets/todo.png';
+import cart from '../assets/Shopping Cart.png';
+import card from '../assets/Profile Card.png';
+import joke from '../assets/joke Generator.png';
+import ip from '../assets/IP Finder.png';
+import submission from '../assets/Submission Form.png';
 import { useEffect } from 'react';
 
 
@@ -58,6 +63,22 @@ const Projects: React.FC = () => {
                 <div className="div">
                     <a href="https://todo-app-woad-one-64.vercel.app/" target="_blank"><img className="project-tile profile-link" src={todo} alt="frank's todo app" /></a>
                     <p>Simple todo App</p>
+                </div>
+                <div className="div">
+                    <a href="https://joke-generator-taupe-ten.vercel.app/" target="_blank"><img className="project-tile profile-link" src={joke} alt="frank's todo app" /></a>
+                    <p>Joke Generator</p>
+                </div>
+                <div className="div">
+                    <a href="https://ip-address-finder-weld.vercel.app/" target="_blank"><img className="project-tile profile-link" src={ip} alt="frank's todo app" /></a>
+                    <p>IP Address Finder</p>
+                </div>
+                <div className="div">
+                    <a href="https://shopping-cart-swart-one.vercel.app/" target="_blank"><img className="project-tile profile-link" src={cart} alt="frank's todo app" /></a>
+                    <p>Shopping Cart</p>
+                </div>
+                <div className="div">
+                    <a href="https://profile-card-ten-mauve.vercel.app/" target="_blank"><img className="project-tile profile-link" src={card} alt="frank's todo app" /></a>
+                    <p>Profile Card</p>
                 </div>
             </div>
     </div>
