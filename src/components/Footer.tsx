0438354ddb55a7fa7d3c0b3053c41138be2af62b
Frank <wanyoike39@gmail.com>
2024-09-08 11:08:33 +0300

    add footer section and css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import '../css/footer.css';
 
 const Footer: React.FC = () => {
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear()} Created by Frank</p>
+            <p>&copy; {new Date().getFullYear()} Frank's Portfolio</p>
         </footer>
     );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,6 @@
-import react from 'react';
+import '../css/projects.css';
+import chess from '../assets/ranci-chess.png';
+import todo from '../assets/todo.png';
 
 const Projects: React.FC = () => {
     return (
@@ -7,15 +9,11 @@ const Projects: React.FC = () => {
             <div className="underscore"></div>
             <div className="project-tiles">
                 <div className="div">
-                    <a href="https://ranci-18.github.io/my-website/" target="_blank"><img className="project-tile profile-link" src="assets/myWebsite.png" alt="frank's Website" /></a>
-                    <p>Frank's Website</p>
-                </div>
-                <div className="div">
-                    <a href="https://github.com/Ranci-18/ranci-chess" target="_blank"><img className="project-tile profile-link" src="assets/ranci-chess.png" alt="frank's chess app" /></a>
+                    <a href="https://github.com/Ranci-18/ranci-chess" target="_blank"><img className="project-tile profile-link" src={chess} alt="frank's chess app" /></a>
                     <p>Ranci-chess App</p>
                 </div>
                 <div className="div">
-                    <a href="https://todo-app-woad-one-64.vercel.app/" target="_blank"><img className="project-tile profile-link" src="../assets/todo.png" alt="frank's todo app" /></a>
+                    <a href="https://todo-app-woad-one-64.vercel.app/" target="_blank"><img className="project-tile profile-link" src={todo} alt="frank's todo app" /></a>
                     <p>Simple todo App</p>
                 </div>
             </div>
