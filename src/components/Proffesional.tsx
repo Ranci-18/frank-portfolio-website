985203342f3e16c2058838bd04f2277fbefe126c
Frank <wanyoike39@gmail.com>
2024-09-08 10:36:46 +0300

    add professional section

--- src/components/Proffesional.tsx
+++ src/components/Proffesional.tsx
@@ -1,12 +1,14 @@
-import react from 'react';
+import cert from '../assets/certificate-francis-ng-ang-a.png';
+import badge from '../assets/ciscoBadge.png';
+import '../css/professional.css';
 
 const Proffesional: React.FC = () => {
     return (
         <div id="professional">
         <h2>Tech profile</h2>
         <div className="tech">
-            <img src="assets/certificate-francis-ng-ang-a.png" alt="frank's certificate" className="cert" ></img>
-            <img src="assets/ciscoBadge.png" alt="cisco junior cyber security badge" className="badge"></img>
+            <img src={cert} alt="frank's certificate" className="cert" ></img>
+            <img src={badge} alt="cisco junior cyber security badge" className="badge"></img>
         </div>
     </div>
     );
