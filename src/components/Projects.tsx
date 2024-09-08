55f7581e681f9fefd119bbad82fcbefebc1a1702
Frank <wanyoike39@gmail.com>
2024-09-08 19:25:37 +0300

    put the auto-scroll logic inside the useEffect

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,43 +1,51 @@
 import '../css/projects.css';
 import chess from '../assets/ranci-chess.png';
 import todo from '../assets/todo.png';
+import { useEffect } from 'react';
 
-const projectTiles = document.querySelector('.project-tiles');
-const tiles = document.querySelectorAll('.project-tiles .div');
 
 let scrollAmount = 0;
-
-function autoScroll() {
-    scrollAmount += 1;
-    if (projectTiles) {
-        projectTiles.scrollLeft = scrollAmount;
-
-        if (scrollAmount >= projectTiles.scrollWidth - projectTiles.clientWidth) {
-            scrollAmount = 0;
-        }
-    }
-    highlightActiveTile();
-}
-
-function highlightActiveTile() {
-    if (projectTiles) {
-        const center = projectTiles.scrollLeft + projectTiles?.clientWidth / 2
-
-        tiles.forEach(tile => {
-            const tileCenter = (tile as HTMLElement).offsetLeft + tile.clientWidth / 2;
-
-            if (Math.abs(center - tileCenter) < tile.clientWidth / 2) {
-                tile.classList.add('active');
-            } else {
-                tile.classList.remove('active');
-            }
-        });
-    }
-}
-
-setInterval(autoScroll, 20);
-
 const Projects: React.FC = () => {
+    useEffect(() => {
+        const projectTiles = document.querySelector('.project-tiles');
+        const tiles = document.querySelectorAll('.project-tiles .div');
+
+
+
+        function autoScroll() {
+            scrollAmount += 1;
+            if (projectTiles) {
+                projectTiles.scrollLeft = scrollAmount;
+
+                if (scrollAmount >= projectTiles.scrollWidth - projectTiles.clientWidth) {
+                    scrollAmount = 0;
+                }
+            }
+            highlightActiveTile();
+        }
+
+        function highlightActiveTile() {
+            if (projectTiles) {
+                const center = projectTiles.scrollLeft + projectTiles.clientWidth / 2
+
+                tiles.forEach(tile => {
+                    const tileCenter = (tile as HTMLElement).offsetLeft + tile.clientWidth / 2;
+
+                    if (Math.abs(center - tileCenter) < tile.clientWidth / 2) {
+                        tile.classList.add('active');
+                    } else {
+                        tile.classList.remove('active');
+                    }
+                });
+            }
+        }
+
+        const intervalId = setInterval(autoScroll, 50);
+
+        return () => {
+            clearInterval(intervalId);}
+    }, []);
+
     return (
         <div id="projects">
             <h2>These are some of my projects</h2>
